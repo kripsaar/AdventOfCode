ed0e0b59b0a0fcae3f2f0296f5a48dedcbc7e5cd
Aivar Kripsaar <aivarkripsaar@gmail.com>
2021-12-07 21:28:22 +0100

    Day 07

---
+++ 2021/07-12/07-12-02.py
@@ -0,0 +1,39 @@
+import math
+
+def parse_input(filename: str):
+    with open(filename, mode="r") as input:
+        line = input.readline()
+        return [int(item) for item in line.split(",")]
+
+def triangular(n: int):
+    return int(n * (n + 1) / 2)
+
+def calc_cost(list, position):
+    cost = 0
+    for item in list:
+        distance = abs(item - position) 
+        cost += triangular(distance)
+    return cost
+
+def step(list, min, max, min_cost, max_cost):
+    if min >= max:
+        return min, min_cost
+    next_pos = math.floor((min + max) / 2)
+    cost = calc_cost(list, next_pos)
+    if min_cost <= max_cost:
+        return step(list, min, next_pos, min_cost, cost)
+    elif min == next_pos:
+        return max, max_cost
+    else:
+        return step(list, next_pos, max, cost, max_cost)
+    
+
+list = parse_input("input-07")
+min = min(list)
+max = max(list)
+cost_min = calc_cost(list, min)
+cost_max = calc_cost(list, max)
+
+final_pos, final_cost = step(list, min, max, cost_min, cost_max)
+print(f"Final position: {final_pos}")
+print(f"Final cost: {final_cost}")
