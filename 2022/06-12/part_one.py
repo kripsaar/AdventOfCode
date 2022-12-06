e4a4761601829c030320e861590c653ed24b4b89
Aivar Kripsaar <aivarkripsaar@gmail.com>
2022-12-06 18:54:02 +0100

    2022 Day 06

---
+++ 2022/06-12/part_one.py
@@ -0,0 +1,23 @@
+
+def parse_input(filename: str):
+    with open(filename, mode='r') as file:
+        return file.readline().strip()
+
+def is_start_marker(marker: str):
+    return len(set(marker)) == 4
+
+def find_message_start(buffer: str):
+    size = len(buffer)
+    start = 0
+    end = 4
+    while end <= size:
+        if is_start_marker(buffer[start:end]):
+            return end
+        start += 1
+        end += 1
+
+
+filename = 'input-06'
+buffer = parse_input(filename)
+start = find_message_start(buffer)
+print(start)
