cb310f73353a53149be618315251f62c4104030f
Aivar Kripsaar <aivarkripsaar@gmail.com>
2022-12-22 23:25:47 +0200

    2022 Day 20

---
+++ 2022/20-12/part_two.py
@@ -0,0 +1,38 @@
+import math
+
+def parse_input(filename: str):
+    numbers = []
+    with open(filename, mode = 'r') as file:
+        for line in file.readlines():
+            number = int(line.strip()) * 811589153
+            numbers.append(number)
+    return numbers
+
+def wrap(numbers: list):
+    result = numbers.copy()
+    list_len = len(numbers)
+    index_list = list(range(0, list_len))
+    for _ in range(0, 10):
+        for i in range(0, list_len):
+            value = numbers[i]
+            old_index = index_list.index(i)
+            new_index = (old_index + value ) % (list_len - 1)
+            del index_list[old_index]
+            del result[old_index]
+            index_list.insert(new_index, i)
+            result.insert(new_index, value)
+    return result
+
+def evaluate(numbers: list):
+    res = []
+    start = numbers.index(0)
+    for i in range(1000, 3001, 1000):
+        res.append(numbers[(start + i) % len(numbers)])
+    return sum(res)
+
+filename = 'input-20'
+numbers = parse_input(filename)
+wrapped = wrap(numbers)
+result = evaluate(wrapped)
+
+print(result)
