2e7b290ebcf92cb81a605f17efad60105036969a
Aivar Kripsaar <aivarkripsaar@gmail.com>
2025-12-06 15:53:36 +0100

    2025 Template

---
+++ AoC-2025/day_template/src/part2.rs
@@ -0,0 +1,14 @@
+pub fn part2() {
+    let _input = include_str!("../resources/input-0");
+    // println!("Part 2: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_example() {
+        let _example_input = include_str!("../resources/input-0-test");
+        todo!()
+    }
+}
