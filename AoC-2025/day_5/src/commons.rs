3be69847acd510ea18fc80fc13ea9ad6a6032cbc
Aivar Kripsaar <aivarkripsaar@gmail.com>
2025-12-06 18:57:27 +0100

    2025 Day 5 Part 2

--- AoC-2025/day_5/src/commons.rs
+++ AoC-2025/day_5/src/commons.rs
@@ -1,10 +1,12 @@
 use std::ops::RangeInclusive;
 use std::str::FromStr;
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Ingredient {
     id: usize,
 }
 
+#[derive(Debug)]
 pub struct FreshnessDatabase {
     freshness_ranges: Vec<RangeInclusive<usize>>,
     ingredients: Vec<Ingredient>,
@@ -58,5 +60,54 @@ impl FreshnessDatabase {
             .iter()
             .filter(|ingredient| self.is_ingredient_fresh(ingredient))
             .count()
+    }
+
+    pub fn count_all_possible_fresh_ingredients(&self) -> usize {
+        let normalized_freshness_database = self.merge_overlapping_ranges();
+        normalized_freshness_database
+            .freshness_ranges
+            .iter()
+            .map(|range| range.end() - range.start() + 1)
+            .sum()
+    }
+
+    pub fn merge_overlapping_ranges(&self) -> FreshnessDatabase {
+        let mut old_freshness_ranges = self.freshness_ranges.clone();
+        let mut new_freshness_ranges: Vec<RangeInclusive<usize>> = Vec::new();
+        old_freshness_ranges.sort_by_key(|range| *range.start());
+        let mut left = old_freshness_ranges.first().unwrap().clone();
+        for right in &old_freshness_ranges[1..] {
+            if left.start() <= right.start() && left.end() >= right.end() {
+                // right is completely inside left
+                continue;
+            }
+            if left.end() >= right.start() && right.end() > left.end() {
+                left = *left.start()..=*right.end()
+            } else {
+                new_freshness_ranges.push(left.clone());
+                left = right.clone();
+            }
+        }
+        new_freshness_ranges.push(left);
+        FreshnessDatabase {
+            freshness_ranges: new_freshness_ranges,
+            ingredients: self.ingredients.clone(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::commons::FreshnessDatabase;
+
+    #[test]
+    fn test_merging_overlapping_ranges() {
+        let ranges = vec![1..=10, 3..=5, 8..=15];
+        let freshness_database = FreshnessDatabase {
+            freshness_ranges: ranges,
+            ingredients: vec![],
+        };
+        let merged = freshness_database.merge_overlapping_ranges();
+        assert_eq!(merged.freshness_ranges, vec![1..=15]);
     }
 }
--- AoC-2025/day_5/src/part2.rs
+++ AoC-2025/day_5/src/part2.rs
@@ -1,14 +1,22 @@
+use crate::commons::FreshnessDatabase;
+use std::str::FromStr;
+
 pub fn part2() {
-    let _input = include_str!("../resources/input-5");
-    // println!("Part 2: {}", result);
+    let input = include_str!("../resources/input-5");
+    let freshness_database = FreshnessDatabase::from_str(input).unwrap();
+    let result = freshness_database.count_all_possible_fresh_ingredients();
+    println!("Part 2: {}", result);
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn test_example() {
-        let _example_input = include_str!("../resources/input-5-test");
-        todo!()
+        let example_input = include_str!("../resources/input-5-test");
+        let freshness_database = FreshnessDatabase::from_str(example_input).unwrap();
+        let result = freshness_database.count_all_possible_fresh_ingredients();
+        assert_eq!(result, 14);
     }
 }
