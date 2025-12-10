186c301e62f4546679d72259c8a7753a3975d060
Aivar Kripsaar <aivarkripsaar@gmail.com>
2025-12-10 21:34:07 +0100

    2025 Day 9 Part 1

---
+++ AoC-2025/day_9/src/main.rs
@@ -0,0 +1,11 @@
+use crate::part1::part1;
+use crate::part2::part2;
+
+mod commons;
+mod part1;
+mod part2;
+
+fn main() {
+    part1();
+    part2();
+}
---
+++ AoC-2025/day_9/src/part2.rs
@@ -0,0 +1,14 @@
+pub fn part2() {
+    let _input = include_str!("../resources/input-9");
+    // println!("Part 2: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_example() {
+        let _example_input = include_str!("../resources/input-9-test");
+        todo!()
+    }
+}
